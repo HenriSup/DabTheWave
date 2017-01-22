ef984256208b713d579c58f540c02cd3415b1c13
noxidream <noxidream@gmail.com>
2017-01-22 06:32:46 +0100

    multiple tracks for difficulty

--- assets/Scripts (5)/ParrotBehavior (40)/script.ts
+++ assets/Scripts (5)/ParrotBehavior (40)/script.ts
@@ -1,7 +1,10 @@
 class ParrotBehavior extends Sup.Behavior {
   
   private shouldCallDab:boolean;
-  
+  private rightShouldPlayOn=0;
+  private leftShouldPlayOn=0;
+  private rightSound = [new Sup.Audio.SoundPlayer("Sounds/ParrotRight", 0.2, { loop: false }),new Sup.Audio.SoundPlayer("Sounds/ParrotRight", 0.2, { loop: false }),new Sup.Audio.SoundPlayer("Sounds/ParrotRight", 0.2, { loop: false })];
+  private leftSound = [new Sup.Audio.SoundPlayer("Sounds/ParrotLeft", 0.2, { loop: false }),new Sup.Audio.SoundPlayer("Sounds/ParrotLeft", 0.2, { loop: false }),new Sup.Audio.SoundPlayer("Sounds/ParrotLeft", 0.2, { loop: false })];
   awake() {
     
   }
@@ -13,6 +16,7 @@ class ParrotBehavior extends Sup.Behavior {
   animate(){
     if(this.shouldCallDab){
       this.actor.spriteRenderer.setAnimation("DabCall",false)
+      this.playSound()
       this.shouldCallDab=false
     }
     if (this.actor.spriteRenderer.getAnimation()=="DabCall" && this.actor.spriteRenderer.getAnimationFrameIndex() >= this.actor.spriteRenderer.getAnimationFrameCount()-1){
@@ -22,6 +26,29 @@ class ParrotBehavior extends Sup.Behavior {
   
   public callDab(){
     this.shouldCallDab = true
+  }
+  
+  public playSound(){
+    var randomNumber = Math.random() >= 0.5
+    if (randomNumber) {
+      this.leftSound[this.leftShouldPlayOn].setVolume(0.3);
+      this.leftSound[this.leftShouldPlayOn].setPitch(0);
+      this.leftSound[this.leftShouldPlayOn].play();
+      this.leftShouldPlayOn++;
+      if (this.leftShouldPlayOn>=this.leftSound.length){
+        this.leftShouldPlayOn=0;
+      }
+    }
+    else {
+      this.rightSound[this.rightShouldPlayOn].setVolume(0.3);
+      this.rightSound[this.rightShouldPlayOn].setPitch(0);
+      this.rightSound[this.rightShouldPlayOn].play();
+      this.rightShouldPlayOn++;
+      if (this.rightShouldPlayOn>=this.rightSound.length){
+        this.rightShouldPlayOn=0;
+      }
+    }
+   
   }
 }
 Sup.registerBehavior(ParrotBehavior);
--- assets/Scripts (5)/TempoBehavior (27)/script.ts
+++ assets/Scripts (5)/TempoBehavior (27)/script.ts
@@ -2,11 +2,15 @@ class TempoBehavior extends Sup.Behavior {
   private frameCounter:number=0
   private timer:number=0
   private beatCounter:number=0
-  private beatPerSec:number=2
+  private beatPerSec:number=2.05
   private oldBeat:number=-1
   private isPlayerTurn:boolean=false
   private shouldPlayOn=0;
-  private hitSoundPlayers = [new Sup.Audio.SoundPlayer("Sounds/BeatSound", 0.8, { loop: false }),new Sup.Audio.SoundPlayer("Sounds/BeatSound", 0.8, { loop: false }),new Sup.Audio.SoundPlayer("Sounds/BeatSound", 0.8, { loop: false })];
+  private measureLeft=2;
+  private track0 = [new Sup.Audio.SoundPlayer("Sounds/track0", 0.8, { loop: false }),new Sup.Audio.SoundPlayer("Sounds/track0", 0.8, { loop: false })];
+  private track1 = [new Sup.Audio.SoundPlayer("Sounds/track1", 0.8, { loop: false }),new Sup.Audio.SoundPlayer("Sounds/track1", 0.8, { loop: false })];
+  private track2 = [new Sup.Audio.SoundPlayer("Sounds/track2", 0.8, { loop: false }),new Sup.Audio.SoundPlayer("Sounds/track2", 0.8, { loop: false })];
+  private track3 = [new Sup.Audio.SoundPlayer("Sounds/track3", 0.8, { loop: false }),new Sup.Audio.SoundPlayer("Sounds/track3", 0.8, { loop: false })];
   awake() {
     
   }
@@ -14,17 +18,8 @@ class TempoBehavior extends Sup.Behavior {
   update() {
     this.updateFrameCounterAndTimerAndBeatCounter()
     //
-    if (Sup.Input.wasKeyJustPressed("LEFT")){
-      var rythmBorder = Sup.appendScene("Prefabs/RythmBorder")[0]
-      rythmBorder.setPosition(this.actor.getPosition().x,this.actor.getPosition().y,5)
-    }
-    if (Sup.Input.wasKeyJustPressed("RIGHT")){
-      var rythmBorder = Sup.appendScene("Prefabs/RythmBorder")[0]
-      rythmBorder.getBehavior(RythmBorderBehavior).setIsPlayerBorder()
-      rythmBorder.setPosition(this.actor.getPosition().x,this.actor.getPosition().y,5)
-    }
+  
     if (this.beatJustChanged()){
-      
       if (this.isPlayerTurn){
         var rythmBorder = Sup.appendScene("Prefabs/RythmBorder")[0]
         rythmBorder.getBehavior(RythmBorderBehavior).setIsPlayerBorder()
@@ -37,8 +32,11 @@ class TempoBehavior extends Sup.Behavior {
         var rythmBorder = Sup.appendScene("Prefabs/RythmBorder")[0];
         rythmBorder.setPosition(this.actor.getPosition().x,this.actor.getPosition().y,5)
       }
+      Sup.log(this.beatCounter%4 + " time: " + this.timer)
       if(this.beatCounter%4==0){
-        this.playSound()
+        if (this.measureLeft<=0){
+          this.playTrack3()
+        } else this.measureLeft-=1
       }
       if ((this.beatCounter%4)+1==4){
         this.swapTurn() 
@@ -55,14 +53,41 @@ class TempoBehavior extends Sup.Behavior {
     else {this.isPlayerTurn=true}
   }
   
-  playSound(){
-    
-   // var random = (Math.random() * (higher-lower)) + lower;
-    this.hitSoundPlayers[this.shouldPlayOn].setVolume(0.5);
-    this.hitSoundPlayers[this.shouldPlayOn].setPitch(0);
-    this.hitSoundPlayers[this.shouldPlayOn].play();
+  playTrack0(){
+    this.track0[this.shouldPlayOn].setVolume(0.5);
+    this.track0[this.shouldPlayOn].setPitch(0);
+    this.track0[this.shouldPlayOn].play();
     this.shouldPlayOn++;
-    if (this.shouldPlayOn>=this.hitSoundPlayers.length){
+    if (this.shouldPlayOn>=this.track0.length){
+      this.shouldPlayOn=0;
+    }
+  }
+  playTrack1(){
+    this.track1[this.shouldPlayOn].setVolume(0.5);
+    this.track1[this.shouldPlayOn].setPitch(0);
+    this.track1[this.shouldPlayOn].play();
+    this.shouldPlayOn++;
+    if (this.shouldPlayOn>=this.track1.length){
+      this.shouldPlayOn=0;
+    }
+  }
+  playTrack2(){
+    this.measureLeft=1
+    this.track2[this.shouldPlayOn].setVolume(0.5);
+    this.track2[this.shouldPlayOn].setPitch(0);
+    this.track2[this.shouldPlayOn].play();
+    this.shouldPlayOn++;
+    if (this.shouldPlayOn>=this.track2.length){
+      this.shouldPlayOn=0;
+    }
+  }
+  playTrack3(){
+    this.measureLeft=1
+    this.track3[this.shouldPlayOn].setVolume(0.5);
+    this.track3[this.shouldPlayOn].setPitch(0);
+    this.track3[this.shouldPlayOn].play();
+    this.shouldPlayOn++;
+    if (this.shouldPlayOn>=this.track3.length){
       this.shouldPlayOn=0;
     }
   }
